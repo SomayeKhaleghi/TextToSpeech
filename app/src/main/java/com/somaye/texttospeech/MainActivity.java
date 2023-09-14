949f7fef8a837f5116ee47b4af0b3fc6408553bf
somaye khaleghi <SomayeKhaleghi@gmail.com>
2023-09-14 20:50:28 +0330

    bind view
    in main activity bind button and text view

--- app/src/main/java/com/somaye/texttospeech/MainActivity.java
+++ app/src/main/java/com/somaye/texttospeech/MainActivity.java
@@ -2,19 +2,29 @@ package com.somaye.texttospeech;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
-
-import android.app.Activity;
 import android.os.Bundle;
-
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import com.somaye.texttospeech.databinding.ActivityMainBinding;
-
+import java.security.KeyStore;
+import java.util.Locale;
+//---------------------------------------------------------------------------------------------------------
 public class MainActivity extends AppCompatActivity {
-
+//---------------------------------------------------------------------------------------------------------
+EditText editTxt;
+TextToSpeech textToSpeech;
+Button btn ;
 ActivityMainBinding binding;
-
+//---------------------------------------------------------------------------------------------------------
 @Override
 protected void onCreate(Bundle savedInstanceState){
     super.onCreate(savedInstanceState);
     binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    editTxt = binding.edittxt;
+    btn =  binding.btn;
+   
 }
+//---------------------------------------------------------------------------------------------------------
 }
